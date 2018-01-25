6959f60538f1b3120dfc534d4cfd48858b97c8ea
Administrator <Administrator@USER-20160814GC>
2018-01-25 22:28:12 +0800

    web模块开发

--- account-web/src/main/java/com/yy/account/web/Captcha.java
+++ account-web/src/main/java/com/yy/account/web/Captcha.java
@@ -1,30 +1,50 @@
 package com.yy.account.web;
 
 import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.yy.account.service.AccountService;
 
 /**
  * Servlet implementation class Captcha
  */
 public class Captcha extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    private ApplicationContext context;
+    @Override
+    public void init() throws ServletException {
+    	super.init();
+    	context= WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		AccountService service=(AccountService) context.getBean("accountService");
+		String captchaKey = service.generateCaptchaKey();
+		try {
+			byte[] image = service.generateCaptchaImage(captchaKey);
+			response.setContentType("image/jpeg");
+			OutputStream out = response.getOutputStream();
+			out.write(image);
+			out.close();
+		} catch (Exception e) {
+			response.sendError(404, e.getMessage());
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- account-web/src/main/webapp/index.jsp
+++ account-web/src/main/webapp/index.jsp
@@ -1,14 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+<head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>用户注册</title>
+<style type="text/css">
+</style>
+</head>
 <body>
-<h2>欢迎注册!</h2>
-<form action="">
-	<label>账户id：</label><input></input>
-	<label>显示名称：</label><input></input>
-	<label>密码：</label><input></input>
-	<label>确认密码：</label><input></input>
-	<label>验证码：</label><input></input>
-	<button type="button">注册</button>
-</form>
+	<h2>用户注册</h2>
+	<form action="">
+		<input placeholder="用户id"><br><br>
+		<input placeholder="用户名称"><br><br>
+		<input placeholder="密码"><br><br>
+		<input placeholder="确认密码"><br><br>
+		<input placeholder="邮箱"><br><br>
+		<input placeholder="验证码"><br><br>
+		<img alt="验证码" src=""><br><br>
+		<input type="submit" value="提交注册">
+	</form>
 </body>
 </html>
